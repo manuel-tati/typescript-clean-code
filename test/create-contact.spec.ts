e18b716560979f5e9feb963d1235eb0a37ba5cec
Manuel Tati <manuel.luemba.tati@atlantico.ao>
2022-07-17 17:48:34 +0100

    do not create contact when phone number is invalid

--- test/create-contact.spec.ts
+++ test/create-contact.spec.ts
@@ -3,13 +3,30 @@ import MockContactsRepository from "./mock/repositories/mock.contact.repository"
 import CreateContactUseCase from "./mock/use-cases/create.contact.use-case";
 
 describe("CREATE CONTACT", () => {
+  test("Should return error if phone number is invalid", () => {
+    const newContact: Contact = {
+      id: 3,
+      firstName: "Tume",
+      lastName: "Kaillan",
+      email: "mazal@test.com",
+      phone: "+244-901-001-001",
+    };
+    const mockContactsRepository = new MockContactsRepository();
+    const createContactUseCase = new CreateContactUseCase(
+      mockContactsRepository
+    );
+
+    expect(() => createContactUseCase.create(newContact)).toThrow(
+      "Invalid phone number"
+    );
+  });
   test("Should validate if contact email is valid", () => {
     const newContact: Contact = {
       id: 3,
       firstName: "Tume",
       lastName: "Kaillan",
       email: "tume.com",
-      phone: "54321",
+      phone: "+244 901 001 001",
     };
     const mockContactsRepository = new MockContactsRepository();
     const createContactUseCase = new CreateContactUseCase(
@@ -25,7 +42,7 @@ describe("CREATE CONTACT", () => {
       firstName: "Tume",
       lastName: "Kaillan",
       email: "mazal@test.com",
-      phone: "54321",
+      phone: "+244 901 001 001",
     };
     const mockContactsRepository = new MockContactsRepository();
     const createContactUseCase = new CreateContactUseCase(
@@ -43,7 +60,7 @@ describe("CREATE CONTACT", () => {
       firstName: "Tume",
       lastName: "Kaillan",
       email: "tume@test.com",
-      phone: "54321",
+      phone: "+244 901 001 001",
     };
     const mockContactsRepository = new MockContactsRepository();
     const createContactUseCase = new CreateContactUseCase(
--- test/mock/use-cases/create.contact.use-case.ts
+++ test/mock/use-cases/create.contact.use-case.ts
@@ -6,14 +6,22 @@ class CreateContactUseCase implements ICreateContactUseCase {
   constructor(private repository: IContactsRepository) {}
 
   create(contact: Contact): void {
-    this.checkIfEmailIsValid(contact.email);
+    this.validateEmail(contact.email);
+    this.validatePhoneNumber(contact.phone);
     this.checkIfEmailAlreadyExists(contact.email, contact.id);
     this.repository.save(contact);
   }
 
-  private checkIfEmailIsValid = (email: string) => {
+  private validateEmail = (email: string) => {
     var pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
     if (String(email).search(pattern)) throw new Error("Invalid email");
+  };
+
+  private validatePhoneNumber = (phoneNumber: string) => {
+    var patternPhoneNumber =
+      /^\+?([0-9]{3})\)?[ . ]?([0-9]{3})[ . ]?([0-9]{3})[ . ]?([0-9]{3})$/;
+    if (String(phoneNumber).search(patternPhoneNumber))
+      throw new Error("Invalid phone number");
   };
 
   private checkIfEmailAlreadyExists = (email: string, id?: number) => {
