944c30fca5e797b696616e77bbd19d5f88a5d443
Manuel Tati <manuel.luemba.tati@atlantico.ao>
2022-07-17 16:46:53 +0100

    check if email is valid

--- test/create-contact.spec.ts
+++ test/create-contact.spec.ts
@@ -3,7 +3,23 @@ import MockContactsRepository from "./mock/repositories/mock.contact.repository"
 import CreateContactUseCase from "./mock/use-cases/create.contact.use-case";
 
 describe("CREATE CONTACT", () => {
-  test("Should return 3 as contacts size", () => {
+  test("Should validate if contact email is valid", () => {
+    const newContact: Contact = {
+      id: 3,
+      firstName: "Tume",
+      lastName: "Kaillan",
+      email: "tume.com",
+      phone: "54321",
+    };
+    const mockContactsRepository = new MockContactsRepository();
+    const createContactUseCase = new CreateContactUseCase(
+      mockContactsRepository
+    );
+
+    expect(() => createContactUseCase.create(newContact)).toThrow();
+  });
+
+  test("Should return 4 as contacts size", () => {
     const newContact: Contact = {
       id: 3,
       firstName: "Tume",
--- test/mock/use-cases/create.contact.use-case.ts
+++ test/mock/use-cases/create.contact.use-case.ts
@@ -6,7 +6,14 @@ class CreateContactUseCase implements ICreateContactUseCase {
   constructor(private repository: IContactsRepository) {}
 
   create(contact: Contact): void {
+    this.checkIfEmailIsValid(contact.email);
     this.repository.save(contact);
   }
+
+  private checkIfEmailIsValid = (email: string) => {
+    var pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+    if (String(email).search(pattern)) throw new Error("Invalid email");
+    return email;
+  };
 }
 export default CreateContactUseCase;
