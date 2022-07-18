e112c19c68fe086377060731f85bf0c6cc9e23be
Manuel Tati <manuel.luemba.tati@atlantico.ao>
2022-07-18 21:48:16 +0100

    create builder to contact model

--- src/domain/models/contact.ts
+++ src/domain/models/contact.ts
@@ -1,7 +1,36 @@
-export type Contact = {
+export interface Contact {
   id: number;
   firstName: string;
   lastName: string;
   email: string;
   phone: string;
-};
+}
+
+export class ContactBuilder {
+  constructor(private readonly contact: Contact) {}
+
+  id(id: number): ContactBuilder {
+    this.contact.id = id;
+    return this;
+  }
+
+  firstName(firstName: string): ContactBuilder {
+    this.contact.firstName = firstName;
+    return this;
+  }
+
+  lastName(lastName: string): ContactBuilder {
+    this.contact.lastName = lastName;
+    return this;
+  }
+
+  email(email: string): ContactBuilder {
+    this.contact.email = email;
+    return this;
+  }
+
+  phone(phone: string): ContactBuilder {
+    this.contact.phone = phone;
+    return this;
+  }
+}
