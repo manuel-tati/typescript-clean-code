bd379999aff2f100628b6f76fd26514dba51307a
Manuel Tati <manuel.luemba.tati@atlantico.ao>
2022-07-18 22:04:13 +0100

    modified contact model in mock

--- test/mock/models/contact.ts
+++ test/mock/models/contact.ts
@@ -1,7 +1,41 @@
-export type Contact = {
+export interface Contact {
   id: number;
   firstName: string;
   lastName: string;
   email: string;
   phone: string;
-};
+}
+
+export class ContactBuilder {
+  private readonly contact!: Contact;
+  constructor() {}
+
+  id(id: number): ContactBuilder {
+    this.contact.id = id;
+    return this;
+  }
+
+  firstName(firstName: string): ContactBuilder {
+    this.contact.firstName = firstName;
+    return this;
+  }
+
+  lastName(lastName: string): ContactBuilder {
+    this.contact.lastName = lastName;
+    return this;
+  }
+
+  email(email: string): ContactBuilder {
+    this.contact.email = email;
+    return this;
+  }
+
+  phone(phone: string): ContactBuilder {
+    this.contact.phone = phone;
+    return this;
+  }
+
+  build(): Contact {
+    return this.contact;
+  }
+}
